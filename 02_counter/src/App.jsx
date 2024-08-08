65881e228cdf6e80054d2e6a35c7533418c05cd4
Akashkr28 <email.akash2010@gmail.com>
2024-08-08 21:36:40 +0530

    code cleaned

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   let [counter, setCounter] = useState(15)
 
-  //let counter = 15
-
   const addValue = () => {
     console.log("onclicked", counter);
-    //counter = counter + 1;
     setCounter(counter + 1)
   }
 
